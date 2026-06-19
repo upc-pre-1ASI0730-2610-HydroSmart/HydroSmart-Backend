df8e94049873aaffdc6c4fc43bb9a47a67bc52c7
YeiShari <lleuxmerveivp@gmail.com>
2026-06-19 00:42:56 -0500

    feat(notifications): add model builder extensions.

---
+++ HydroSmart.API/Notifications/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using HydroSmart.API.Notifications.Domain.Model.Aggregates;
+using Microsoft.EntityFrameworkCore;
+
+namespace HydroSmart.API.Notifications.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void ApplyNotificationsConfiguration(this ModelBuilder builder)
+    {
+        // Apply Notification entity configuration
+        builder.ApplyConfiguration(new NotificationConfiguration());
+    }
+}
